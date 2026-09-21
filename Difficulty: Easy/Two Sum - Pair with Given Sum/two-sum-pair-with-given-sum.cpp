e07658a00d40205b6044e8b2bc05fd92d4c86bd1
Ayush Pal <ayushpal2414@gmail.com>
2026-09-21 16:18:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Two Sum - Pair with Given Sum/two-sum-pair-with-given-sum.cpp
+++ Difficulty: Easy/Two Sum - Pair with Given Sum/two-sum-pair-with-given-sum.cpp
@@ -6,12 +6,12 @@ class Solution {
         unordered_map<int,int> mp;
         for(int i=0;i<n;i++){
             int complement=target-arr[i];
-            if(mp.find(complement)!=mp.end()){
+            if(mp.find(complement) != mp.end()) {
                 return true;
             }
             mp[arr[i]]=i;
         }
         return false;
+        
     }
-    
 };
